95edd195ec67132246d341a5232a42486463d33c
Woundrite <ndbendale2004@gmail.com>
2025-01-25 19:39:16 +0530

    Working Backend

--- src/main.py
+++ src/main.py
@@ -1,55 +1,58 @@
-import json
 from openpyxl import load_workbook
 import datetime as dt
 import os
 import xlsxwriter
-import xlwings as xw
+import json
+from xlsx2html import xlsx2html
+import asyncio
+from pyppeteer import launch
+
+async def generate_pdf(url, pdf_path):
+	browser = await launch(headless=True, executablePath=os.getcwd()+'/chrome-win/chrome.exe')
+	page = await browser.newPage()
+	
+	await page.goto(url)
+	
+	await page.pdf({'path': pdf_path, 'format': 'A4', 'printBackground': True})
+	
+	await browser.close()
+
 def to_pdf(file_path):
-	excel_app = xw.App(visible=False)
-	excel_book = excel_app.books.open(file_path)
-	excel_book.sheets[0].to_pdf(path=file_path.replace('.xlsx', '.pdf'), layout=None, show=False, quality='standard')
-	excel_book.save()
-	excel_book.close()
-	excel_app.quit()
-	# # Open Microsoft Excel 
-	# excel = client.Dispatch("Excel.Application") 
-	
-	# # Read Excel File 
-	# sheets = excel.Workbooks.Open(file_path) 
-	# work_sheets = sheets.Worksheets[0] 
-	
-	# # Convert into PDF File 
-	# work_sheets.ExportAsFixedFormat(0, file_path.replace('.xlsx', '.pdf')) 
+	print(f"Generating PDF for of {file_path}...")
+	path =  os.getcwd().replace('\\','/') + '/'
+	file_path = file_path.replace('\\', '/')
+	xlsx2html(path+file_path, 'file.html')
+	asyncio.run(generate_pdf(f'file:///{path+'file.html'}', path+file_path.replace('.xlsx', '.pdf')))
 
 def handle_excel_write(file_path, personal_data):
-
 	# Create a workbook and add a worksheet.
 	workbook = xlsxwriter.Workbook(file_path)
 	worksheet = workbook.add_worksheet()
-
+	# Add a bold format to use to highlight cells.
+	bold = workbook.add_format({'bold': True})
+	# add a blue background to the headers.
+	header_format = workbook.add_format({'bold': True, 'bg_color': '#95b3d7', 'align': 'center', 'valign': 'vcenter', 'border': 1})
+	# add border to the cells.
+	cell_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter'})
+	worksheet.write('A1', 'UAN NUMBER', header_format)
+	worksheet.write('B1', 'MEMBER ID', header_format)
+	worksheet.write('C1', 'ESTABLISHMENT DETAILS', header_format)
+	worksheet.write('D1', 'NAME', header_format)
+	worksheet.write('E1', 'FATHER OR HUSBAND \n NAME', header_format)
+	worksheet.write('F1', 'DATE OF JOIN', header_format)
+	worksheet.write('G1', 'DATE OF EXIT PF', header_format)
 	last_row = 1
 	# Start from the first cell below the headers.
 	for row, index in enumerate(personal_data): # has the following format: [str(uan), row['MemID'], est, Name, FName, dt.datetime.strptime(row["EPFDOJ"], '%d-%b-%Y'), ""]
 		for col, item in enumerate(index):
 			if col >= 5:
 				if type(item) == str:
-					worksheet.write(row + 1, col, item)
+					worksheet.write(row + 1, col, item, cell_format)
 				else:
-					worksheet.write(row + 1, col, item.strftime('%d-%b-%Y'))
+					worksheet.write(row + 1, col, item.strftime('%d-%b-%Y'), cell_format)
 			else:
-				worksheet.write(row + 1, col, item)
+				worksheet.write(row + 1, col, item, cell_format)
 		last_row += 1
-
-	worksheet.add_table(f'A1:G{last_row}', {'autofilter': False, 'banded_columns': False, 'banded_rows': False, 
-											'style': 'Table Style Medium 2',
-											'columns': [{'header': 'UAN NUMBER'},
-														{'header': 'MEMBER ID'},
-														{'header': 'ESTABLISHMENT DETAILS'},
-														{'header': 'NAME'},
-														{'header': 'FATHER OR HUSBAND \n NAME'},
-														{'header': 'DATE OF JOIN'},
-														{'header': 'DATE OF EXIT PF'},
-														]}, )
 	worksheet.autofit()
 
 	workbook.close()
@@ -68,8 +71,6 @@ def handle_input(file_path, uan, root, uan_name_map, company_names_map):
 							min_col=0,
 							max_col=14,
 							values_only=True):
-		# print('----', file_path, '----')
-		# print(row, len(row))
 		personal_data.update({row[0]: {'MemID': row[0], 
 									   'EstID': row[1],
 									   'EPFDOJ': row[2], 
@@ -83,7 +84,6 @@ def handle_input(file_path, uan, root, uan_name_map, company_names_map):
 									   'BalSt': row[10],
 									   'NEMem': row[11],
 									   'UANLink': row[12],}})
-
 	for row in personal_data.values():
 		name = uan_name_map.get(uan, uan)
 		if type(name) == dict:
@@ -95,15 +95,16 @@ def handle_input(file_path, uan, root, uan_name_map, company_names_map):
 
 		est = company_names_map.get(row["EstID"], row["EstID"])
 		
+		# print(output)
+
 		if row["EPFDOE"].strip() == "":
 			output.append([str(uan), row['MemID'], est, Name, FName, dt.datetime.strptime(row["EPFDOJ"], '%d-%b-%Y'), ""])
 		else:
 			output.append([str(uan), row['MemID'], est, Name, FName, dt.datetime.strptime(row["EPFDOJ"], '%d-%b-%Y'), dt.datetime.strptime(row["EPFDOE"], '%d-%b-%Y')])
 	output = sorted(output, key=lambda x: x[5])[::-1]
-	print("___________")
 	handle_excel_write(os.path.join(root, f"{uan}.xlsx"), output)
-	print("___________")
 	workbook.close()
+	return output
 
 def compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file, in_root, out_root):
 	if not os.path.exists(out_root):
@@ -119,7 +120,7 @@ def compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file,
 							values_only=True):
 		company_names_map.update({row[0]:  row[1]})
 
-	# print(json.dumps(company_names_map))
+	print(json.dumps(company_names_map, indent=2))
 	company_name_workbook.close()
 
 
@@ -130,9 +131,8 @@ def compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file,
 							min_col=0,
 							max_col=1,
 							values_only=True):
-		# print("row")
 		uan_id_map.append(row[0])
-	# print(json.dumps(uan_id_map))
+	print(json.dumps(uan_id_map, indent=2))
 	uan_id_workbook.close()
 
 	uan_name_workbook = load_workbook(filename=uan_name_compiled_file)
@@ -142,10 +142,12 @@ def compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file,
 							min_col=2,
 							max_col=4,
 							values_only=True):
-		uan_name_map.update({row[0]: {'Name': row[1], 'FName': row[2]}})
+		uan_name_map.update({str(row[0]): {'Name': row[1], 'FName': row[2]}})
+	print(json.dumps(uan_name_map, indent=2))
 	uan_name_workbook.close()
 
 
+	data = []
 	for root, dirs, files in os.walk(in_root):
 		path = root.split(os.sep)
 		for file in files:
@@ -154,17 +156,60 @@ def compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file,
 					print(f"Processing {file}...")
 				else:
 					print(f"UAN ID not found for {file}...")
-				handle_input(os.path.join(root, file), file[-17:-5], out_root, uan_name_map, company_names_map)
+				data.append(handle_input(os.path.join(root, file), file[-17:-5], out_root, uan_name_map, company_names_map))
+	
+	# convert all xlsx files to pdf
 	for root, dirs, files in os.walk(out_root):
 		for file in files:
 			if file.endswith(".xlsx"):
 				to_pdf(os.path.join(root, file))
+	
+	combine_xlsx(out_root, data)
+
+def combine_xlsx(out_root, data):
+	print("Combining xlsx files...")
+	# reads all the xlsx files in the directory and combines all the data into one xlsx file and saves it, uses xlsx writer
+	# seperate tables for each uan id
+	# Create a workbook and add a worksheet.
+	workbook = xlsxwriter.Workbook(os.path.join(out_root, "Excel Reports.xlsx"))
+	worksheet = workbook.add_worksheet()
+	# header's format.
+	header_format = workbook.add_format({'bold': True, 'bg_color': '#95b3d7','align': 'center', 'valign': 'vcenter', 'border': 1})
+	# add border to the cells.
+	cell_format = workbook.add_format({'border': 1, 'align': 'center', 'valign': 'vcenter'})
+	row = 0
+	col = 0
+	for group in data:
 		
+		worksheet.write(row, 0, 'UAN NUMBER', header_format)
+		worksheet.write(row, 1, 'MEMBER ID', header_format)
+		worksheet.write(row, 2, 'ESTABLISHMENT DETAILS', header_format)
+		worksheet.write(row, 3, 'NAME', header_format)
+		worksheet.write(row, 4, 'FATHER OR HUSBAND \n NAME', header_format)
+		worksheet.write(row, 5, 'DATE OF JOIN', header_format)
+		worksheet.write(row, 6, 'DATE OF EXIT PF', header_format)
+		for r in group:
+			for col, value in enumerate(r):
+				worksheet.write(row + 1, col, value, cell_format)
+			row += 1
+			col = 0
+	worksheet.autofit()
+
+	workbook.close()
+	os.remove("file.html")
+	print("Excel Reports.xlsx has been created.")
 
 if __name__ == "__main__":
-	company_name_map_file = "sample/CompDat.xlsx" # input("Enter the path to the Company name map file: ")
-	uan_id_compiled_file = "sample/UAN.xlsx" # input("Enter the path to the UAN ID compiled file: ")
-	uan_name_compiled_file = "sample/UANFName.xlsx" # input("Enter the path to the UAN Name compiled file: ")
-	in_root = "sample/in" # input("Enter the base directory path: ")
-	out_root = "sample/outers" # input("Enter the out directory path: ")
+	# company_name_map_file = "sample/CompData.xlsx"
+	# uan_id_compiled_file = "sample/UAN.xlsx"
+	# uan_name_compiled_file = "sample/UANFName.xlsx"
+	# in_root = "sample/in"
+	# out_root = "sample/outs"
+	company_name_map_file = input("Enter the path to the Company name map file: ")
+	uan_id_compiled_file = input("Enter the path to the UAN ID compiled file: ")
+	uan_name_compiled_file = input("Enter the path to the UAN Name compiled file: ")
+	in_root = input("Enter the base directory path: ")
+	out_root = input("Enter the out directory path: ")
 	compute(company_name_map_file, uan_id_compiled_file, uan_name_compiled_file, in_root, out_root)
+	
+	print("Completed.")
